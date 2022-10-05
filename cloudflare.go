d4693a0d27a3cac32af6d4f506cc2186633f4a15
chrishic <chris@chrishic.com>
2022-10-05 15:58:02 -0700

    Update IP list with most recent CloudFlare IP ranges

--- cloudflare.go
+++ cloudflare.go
@@ -1,51 +1,53 @@
 package ipcat
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+    "bytes"
+    "fmt"
+    "io/ioutil"
+    "net/http"
 )
 
+//  Ref: Cloudflare IP addresses
+//  https://developers.cloudflare.com/fundamentals/get-started/concepts/cloudflare-ip-addresses/
 var (
-	cloudflareDownload = "https://www.cloudflare.com/ips-v4"
+    cloudflareDownload = "https://www.cloudflare.com/ips-v4"
 )
 
 // DownloadCloudflare downloads the latest Cloudflare IP ranges list
 func DownloadCloudflare() ([]byte, error) {
-	resp, err := http.Get(cloudflareDownload)
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("Failed to download Cloudflare ranges: status code %s", resp.Status)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	resp.Body.Close()
+    resp, err := http.Get(cloudflareDownload)
+    if err != nil {
+        return nil, err
+    }
+    if resp.StatusCode != 200 {
+        return nil, fmt.Errorf("Failed to download Cloudflare ranges: status code %s", resp.Status)
+    }
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return nil, err
+    }
+    resp.Body.Close()
 
-	return bytes.TrimSpace(body), nil
+    return bytes.TrimSpace(body), nil
 }
 
 // UpdateCloudflare parses the Cloudflare IP text file and updates the interval set
 func UpdateCloudflare(ipmap *IntervalSet, body []byte) error {
-	const (
-		cloudflareName = "Cloudflare Inc"
-		cloudflareURL  = "https://www.cloudflare.com/"
-	)
+    const (
+        cloudflareName = "Cloudflare Inc"
+        cloudflareURL  = "https://www.cloudflare.com/"
+    )
 
-	// delete all existing records
-	ipmap.DeleteByName(cloudflareName)
+    // delete all existing records
+    ipmap.DeleteByName(cloudflareName)
 
-	// and add back
-	for _, cidr := range bytes.Split(body, []byte("\n")) {
-		err := ipmap.AddCIDR(string(cidr), cloudflareName, cloudflareURL)
-		if err != nil {
-			return err
-		}
-	}
+    // and add back
+    for _, cidr := range bytes.Split(body, []byte("\n")) {
+        err := ipmap.AddCIDR(string(cidr), cloudflareName, cloudflareURL)
+        if err != nil {
+            return err
+        }
+    }
 
-	return nil
+    return nil
 }
